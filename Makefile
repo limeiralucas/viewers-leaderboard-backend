c5b90ad9e5a2eb3d76502171a32fb44981e293ae
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-06 16:31:23 -0300

    style: added black as a formatter

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 dev:
 	pdm run uvicorn src.viewers_leaderboard_backend.main:app --reload
+format:
+	pdm run black ./src
--- src/viewers_leaderboard_backend/main.py
+++ src/viewers_leaderboard_backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-  return {"status": "ok"}
+    return {"status": "ok"}
