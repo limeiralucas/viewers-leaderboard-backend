c2ec6f8ac23f07b8b27302106f0b31e64557abf1
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-11 18:22:14 -0300

    feat: support env files and multiple environments

--- src/viewers_leaderboard/settings.py
+++ src/viewers_leaderboard/settings.py
@@ -1,15 +1,20 @@
+from os import getenv
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+def get_env_filename():
+    env = getenv("ENV")
+    return f".env.${env.lower()}" if env else ".env"
 
 class Settings(BaseSettings):
     app_name: str = "Viewers Leaderboard Backend"
+    env: str = "dev"
     client_id: str
     client_secret: str
     mongo_conn_str: str
     mongo_db_name: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=get_env_filename())
 
 
 @lru_cache
