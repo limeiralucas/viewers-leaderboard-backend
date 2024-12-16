e70066484db22c432f1f96d96e220599a4dad101
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-16 20:46:34 -0300

    feat: creating twitch client from credentials

--- src/viewers_leaderboard/twitch/client.py
+++ src/viewers_leaderboard/twitch/client.py
@@ -5,7 +5,7 @@ from src.viewers_leaderboard.settings import get_settings
 def get_twitch_client():
     settings = get_settings()
 
-    return Client(
-        token=settings.app_access_token,
+    return Client.from_client_credentials(
+        client_id=settings.app_client_id,
         client_secret=settings.app_client_secret,
     )
