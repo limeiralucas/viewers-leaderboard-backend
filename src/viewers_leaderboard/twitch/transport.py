5bd034db8bc659be5ae0e6e055f83fc3f45291ec
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-16 20:46:58 -0300

    feat: added  transport models for twitch module

--- src/viewers_leaderboard/twitch/transport.py
+++ src/viewers_leaderboard/twitch/transport.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel
 
+
 class AuthUserTokenResponse(BaseModel):
     access_token: str
     refresh_token: str
+    expires_in: int
+    token_type: str
+
+
+class TokenValidationResponse(BaseModel):
+    client_id: str
+    login: str
+    scopes: list[str] | None
+    user_id: str
+    expires_in: int
+
+
+class AppTokenResponse(BaseModel):
+    access_token: str
     expires_in: int
     token_type: str
